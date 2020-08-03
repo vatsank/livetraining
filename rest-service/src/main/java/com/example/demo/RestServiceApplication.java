c7657ba750b180f8b6628a77fcccc49ac36bf2e1
Srivatsan <'vatsank@gmail.com'>
2020-08-03 17:01:31 +0530

    Update

--- rest-service/src/main/java/com/example/demo/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/demo/RestServiceApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.CibilScore;
+import com.example.demo.repos.CibilScoreRepository;
 
 @SpringBootApplication
 public class RestServiceApplication {
@@ -10,4 +16,22 @@ public class RestServiceApplication {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			CibilScoreRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new CibilScore(101, "aa11", "Ramesh", 620));
+				
+				repo.save(new CibilScore(101, "bb22", "Suresh", 670));
+				
+			}
+		};
+	}
 }
