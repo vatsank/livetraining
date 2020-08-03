92c350b9985b723d47ca0701e533ae1f2e4b83cd
Srivatsan <'vatsank@gmail.com'>
2020-08-03 12:37:39 +0530

    Update

--- cibil-score-service/src/main/java/com/example/demo/CibilScoreServiceApplication.java
+++ cibil-score-service/src/main/java/com/example/demo/CibilScoreServiceApplication.java
@@ -2,12 +2,54 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.CibilScore;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class CibilScoreServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CibilScoreServiceApplication.class, args);
+	ConfigurableApplicationContext ctx=	SpringApplication.run(CibilScoreServiceApplication.class, args);
+	
+	   // CibilScore score =(CibilScore) ctx.getBean("cibilScore");
+
+	//    CibilScore score = ctx.getBean(CibilScore.class);
+
+         CibilScore score = ctx.getBean("shyamScore",CibilScore.class);
+
+	    log.info(score.toString());
+	    
 	}
 
+
+	@Bean
+	public CibilScore ramScore() {
+		
+		// Setter DI
+		
+	   CibilScore score = new CibilScore();
+	   
+	       score.setId(20);
+	       score.setPanNumber("aa11");
+	       
+	       return score;
+	}
+	
+	@Bean
+	public CibilScore shyamScore() {
+		
+		// Constructor DI
+		
+	   CibilScore score = new CibilScore(202,"bb22","Shyam",549);
+	   
+	      
+	       
+	       return score;
+	}
+	
 }
