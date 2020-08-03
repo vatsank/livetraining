18f676ff8dae9d6f1db8cdb000151f719464f266
Srivatsan <'vatsank@gmail.com'>
2020-08-03 16:45:32 +0530

    Update

---
+++ rest-service/src/main/java/com/example/demo/entity/CibilScore.java
@@ -0,0 +1,45 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Component;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Data
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "cibilscore_august")
+public class CibilScore {
+
+	 @Id
+	 int  id;
+	 String panNumber;
+	 String firstName;
+	 double score;
+	public CibilScore(int id, String panNumber, String firstName, double score) {
+		super();
+		log.info("==== ALL Arg Constructor Called ====");
+
+		this.id = id;
+		this.panNumber = panNumber;
+		this.firstName = firstName;
+		this.score = score;
+	}
+	public CibilScore() {
+		super();
+
+		log.info("==== No Arg Constructor Called ====");
+	}
+
+}
