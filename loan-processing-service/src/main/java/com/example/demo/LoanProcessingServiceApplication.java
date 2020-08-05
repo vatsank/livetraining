4882db2477464b0e0dac9a81e07d88fce8392c53
Srivatsan <'vatsank@gmail.com'>
2020-08-05 12:17:04 +0530

    Update

---
+++ loan-processing-service/src/main/java/com/example/demo/LoanProcessingServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoanProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProcessingServiceApplication.class, args);
+	}
+
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate template() {
+		
+		return new RestTemplate();
+	}
+}
---
+++ loan-processing-service/src/main/java/com/example/demo/controllers/LoanProcessingController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class LoanProcessingController {
+
+	
+	@Autowired
+	private RestTemplate template;
+	
+	
+	@GetMapping(path = "/api/v1/tax")
+	public String getTaxDetails() {
+
+		// String url = "http://localhost:2020/api/v1/taxdetails";
+		
+		/*
+		 * Since Service Registry and Discovery Pattern is used
+		 * we pass service id instead of host and port name 
+		 * see difference between line 21 and 28 
+		 */
+		String url = "http://TAX-DETAIL-SERVICE/api/v1/taxdetails";
+		
+		
+		return this.template.getForObject(url, String.class);
+		
+	}
+}
