b1f7231d4e890f9230dc36f1c2830d993cb83c51
Srivatsan <'vatsank@gmail.com'>
2020-08-05 11:40:54 +0530

    Producer Example Added

---
+++ tax-details-service/src/main/java/com/example/demo/entity/TaxDetails.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "taxdetails_august")
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaxDetails {
+
+	 @Id
+	 String panNumber;
+	 String entityName;
+	 String entityType;
+	 String aadharNumber;
+}
