7bf861e3059ad66b7ba1ec228dc1d910b1719a4d
Srivatsan <'vatsank@gmail.com'>
2020-08-06 15:11:17 +0530

    Update

--- tax-details-service/src/main/java/com/example/demo/TaxDetailsServiceApplication.java
+++ tax-details-service/src/main/java/com/example/demo/TaxDetailsServiceApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +19,18 @@ public class TaxDetailsServiceApplication {
 	}
 
 
-
+//	@Bean
+//	public  IClientConfig config() {
+//		
+//		return new DefaultClientConfigImpl();
+//	};
+//	
+//	
+//	@Bean
+//	public IRule rule(IClientConfig config) {
+//	
+//		//return new BestAvailableRule();
+//		
+//		return new RoundRobinRule();
+//	}
 }
