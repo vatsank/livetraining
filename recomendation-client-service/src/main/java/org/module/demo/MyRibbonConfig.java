7bd8d1374a692b9e4b0c4346670e4fa5c01045b5
Srivatsan <'vatsank@gmail.com'>
2020-08-06 14:37:19 +0530

    Update

--- recomendation-client-service/src/main/java/org/module/demo/MyRibbonConfig.java
+++ recomendation-client-service/src/main/java/org/module/demo/MyRibbonConfig.java
@@ -1,5 +1,23 @@
 package org.module.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.*;
+@Configuration
 public class MyRibbonConfig {
 
+	@Autowired
+	private IClientConfig config;
+	
+	
+	@Bean
+	public IRule rule() {
+	
+		return new BestAvailableRule();
+		
+	}
 }
