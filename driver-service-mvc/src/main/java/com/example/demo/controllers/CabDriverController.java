4c5eb1ca80259ce771aacd069a39d59bce6e5dfa
Srivatsan <'vatsank@gmail.com'>
2020-09-06 10:39:18 +0530

    Update Controllers

--- driver-service-mvc/src/main/java/com/example/demo/controllers/CabDriverController.java
+++ driver-service-mvc/src/main/java/com/example/demo/controllers/CabDriverController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Driver;
 
 @Controller
 public class CabDriverController {
 
 	
+	
+	@Autowired
+	private Driver driver;
 	
 	@RequestMapping("/")
 	public String init(Model model) {
@@ -16,4 +25,17 @@ public class CabDriverController {
 		return "index";
 	}
 
+	@GetMapping("/addDriver")
+	public String sendForm(Model model) {
+		
+		model.addAttribute("command", driver);
+		return "addCabDriver";
+	}
+	
+	@PostMapping("/donors")
+	public String onSubmit(@ModelAttribute("command") Driver driver) {
+		
+		
+		return "showResult";
+	}
 }
