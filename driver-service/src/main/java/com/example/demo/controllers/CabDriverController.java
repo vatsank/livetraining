276f2c9c900a76eb91d7787ab65ed8492266af04
Srivatsan <'vatsank@gmail.com'>
2020-09-12 09:54:33 +0530

    Updated CRUD Example

--- driver-service/src/main/java/com/example/demo/controllers/CabDriverController.java
+++ driver-service/src/main/java/com/example/demo/controllers/CabDriverController.java
@@ -2,9 +2,19 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.model.Driver;
 import com.example.demo.services.CabDriverService;
@@ -22,4 +32,42 @@ public class CabDriverController {
 		
 		return this.service.getAllDriver();
 	}
+	
+	@GetMapping(path = "/api/v1/drivers/sorted/{key}",produces = "application/json")
+	public List<Driver> sortedList(@PathVariable("key") String key){
+		
+		return this.service.sortedList(key);
+	}
+	
+	@PostMapping(path = "/api/v1/drivers",consumes="application/json",
+			                              produces = "application/json")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Driver addDriver(@RequestBody Driver entity){
+		
+		return this.service.addDriver(entity);
+	}
+	
+	
+	@PutMapping(path = "/api/v1/drivers",consumes="application/json",
+            produces = "application/json")
+    public Driver updateDriver(@RequestBody Driver entity){
+
+     return this.service.updateDriver(entity);
+    }
+	
+	
+	@DeleteMapping(path = "/api/v1/drivers",consumes="application/json",
+            produces = "application/json")
+public Driver deleteDriver(@RequestBody Driver entity,HttpServletResponse resp){
+
+            Driver driver = this.service.deleteDriver(entity);
+            
+            if(driver==null) {
+            	
+            	throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Requested Entity Not Found in table");
+            } 
+            
+            return driver;
+}
+	
 }
--- driver-service/src/main/java/com/example/demo/services/CabDriverService.java
+++ driver-service/src/main/java/com/example/demo/services/CabDriverService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Driver;
@@ -21,4 +22,36 @@ public class CabDriverService {
 		return this.repo.findAll();
 	}
 
+	public Driver addDriver(Driver entity) {
+		
+		return this.repo.save(entity);
+	}
+	
+	
+	public Driver updateDriver(Driver entity) {
+		
+		return this.addDriver(entity);
+	}
+	
+	
+	public Driver deleteDriver(Driver entity) {
+		
+		Driver deleted = null;
+		
+		if(repo.existsById(entity.getDriverId())) {
+			
+			repo.delete(entity);
+			
+			deleted = entity;
+		} 
+		
+		return deleted;
+	}
+	
+	public List<Driver> sortedList(String propName){
+		
+		
+		   return this.repo.findAll(Sort.by(propName).descending());
+		   
+	}
 }
