99ee2d4e7b62406d9087ac73596f97687c7a8434
Srivatsan <'vatsank@gmail.com'>
2020-09-19 11:22:42 +0530

    Update

--- customer_service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer_service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
