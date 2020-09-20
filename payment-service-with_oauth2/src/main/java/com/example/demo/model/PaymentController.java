912fcc199b69e05d86d35b83c91f830f7eb76d50
Srivatsan <vatsank@gmail.com>
2020-09-20 10:16:35 +0530

    Update

--- payment-service-with_oauth2/src/main/java/com/example/demo/model/PaymentController.java
+++ payment-service-with_oauth2/src/main/java/com/example/demo/model/PaymentController.java
@@ -1,5 +1,23 @@
 package com.example.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PaymentController {
 
+	@Autowired
+	Payment payment;
+	
+	@GetMapping("/api/customer/{id}")
+	public Payment getPayment(@PathVariable("id") int id) {
+		
+		payment.setId(id);
+		payment.setAmount(4500);
+		payment.setCustomerName("Ramesh");
+		
+		return payment;
+	}
 }
