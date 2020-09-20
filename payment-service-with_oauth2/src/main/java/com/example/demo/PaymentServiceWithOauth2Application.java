91de39231389e2f519b8f277664b6d0cef25cb95
Srivatsan <vatsank@gmail.com>
2020-09-20 10:05:48 +0530

    Update

---
+++ payment-service-with_oauth2/src/main/java/com/example/demo/PaymentServiceWithOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceWithOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceWithOauth2Application.class, args);
+	}
+
+}
